2d356161e351f9e5d044709456e15b5685ee07d6
1jannerajala <32102867+1jannerajala@users.noreply.github.com>
2023-09-18 12:58:42 +0300

    Update main.tf

--- app-service-and-sql-database/main.tf
+++ app-service-and-sql-database/main.tf
@@ -47,3 +47,14 @@ resource "azurerm_mssql_server" "test" {
 resource "azurerm_mssql_database" "test" {
   name                = "terraform-sqldatabase"
   server_id           = "azurerm_mssql_server.test.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "S0"
+  zone_redundant = true
+
+  tags = {
+    foo = "bar"
+  }
+}
